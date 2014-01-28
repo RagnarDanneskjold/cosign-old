926da5d7ef5188035b7d02621e9723e8e9e48c71
Ryan X. Charles <ryan@bitpay.com>
2014-01-28 16:44:10 -0500

    added basic "it works" test for all validate.x commands
    ...all these tests check is that the proper functions are called when you issue
    the command from the command line. The tests do not check for proper
    functionality.

--- test/Cosign.unit.js
+++ test/Cosign.unit.js
@@ -43,5 +43,113 @@ describe('Cosign', function(){
         fs.writeFile.calledOnce.should.equal(true);
       });
     });
+
+    describe('validate.all', function() {
+      it('should call cmd_validate_all', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.all"]);
+        cosign.cmd_validate_all = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_all.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.fees', function() {
+      it('should call cmd_validate_fees', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.fees"]);
+        cosign.cmd_validate_fees = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_fees.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.inputs', function() {
+      it('should call cmd_validate_inputs', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.inputs"]);
+        cosign.cmd_validate_inputs = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_inputs.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.inputtxs', function() {
+      it('should call cmd_validate_inputtxs', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.inputtxs"]);
+        cosign.cmd_validate_inputtxs = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_inputtxs.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.outputs', function() {
+      it('should call cmd_validate_outputs', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.outputs"]);
+        cosign.cmd_validate_outputs = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_outputs.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.p2sh', function() {
+      it('should call cmd_validate_p2sh', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.p2sh"]);
+        cosign.cmd_validate_p2sh = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_p2sh.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.pubkeys', function() {
+      it('should call cmd_validate_pubkeys', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.pubkeys"]);
+        cosign.cmd_validate_pubkeys = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_pubkeys.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.redeem', function() {
+      it('should call cmd_validate_redeem', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.redeem"]);
+        cosign.cmd_validate_redeem = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_redeem.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('validate.sigs', function() {
+      it('should call cmd_validate_sigs', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","validate.sigs"]);
+        cosign.cmd_validate_sigs = sinon.spy();
+        cosign.main();
+        cosign.cmd_validate_sigs.calledOnce.should.equal(true);
+      });
+    });
   });
 });
