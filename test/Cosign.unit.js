d068992419864896a32782373a82f55ec5860d32
Ryan X. Charles <ryan@bitpay.com>
2014-01-28 18:51:54 -0500

    add basic tests for raw.sign and raw.create
    ...the test does not check for correctness of cmd_raw_sign and cmd_raw_create,
    but rather ensures that the proper functions were called with the respective
    command line arguments.

--- test/Cosign.unit.js
+++ test/Cosign.unit.js
@@ -32,6 +32,32 @@ describe('Cosign', function(){
       });
     });
 
+    describe('raw.create', function() {
+      it('should call cmd_raw_create', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","raw.create"]);
+        cosign.cmd_raw_create = sinon.spy();
+        cosign.main();
+        cosign.program.args[0].should.equal("raw.create");
+        cosign.cmd_raw_create.calledOnce.should.equal(true);
+      });
+    });
+
+    describe('raw.sign', function() {
+      it('should call cmd_raw_sign', function() {
+        var fs = {};
+        fs.readFileSync = function(){return "{}";};
+        var Cosign2 = require('../bin/Cosign').createClass({fs: fs});
+        var cosign = new Cosign2(["","","raw.sign"]);
+        cosign.cmd_raw_sign = sinon.spy();
+        cosign.main();
+        cosign.program.args[0].should.equal("raw.sign");
+        cosign.cmd_raw_sign.calledOnce.should.equal(true);
+      });
+    });
+
     describe('init', function() {
       it('should init the database file', function() {
         var fs = {};
